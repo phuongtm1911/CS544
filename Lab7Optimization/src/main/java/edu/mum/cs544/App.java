f153afd889145f6bd1b2132886470db2dd12f2b3
Minh Phuong Tran <phuongtm1911@gmail.com>
2019-07-01 17:57:11 -0500

    Add Lab7

---
+++ Lab7Optimization/src/main/java/edu/mum/cs544/App.java
@@ -0,0 +1,41 @@
+package edu.mum.cs544;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+public class App {
+
+    private static EntityManagerFactory emf;
+
+    public static void main(String[] args) throws Exception {
+        emf = Persistence.createEntityManagerFactory("cs544");
+
+        long start = System.nanoTime();
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+//        EntityGraph<Owner> graph = em.createEntityGraph(Owner.class);
+//        graph.addAttributeNodes("name");
+//        graph.addSubgraph("pets").addAttributeNodes("name");
+//        TypedQuery<Owner> query = em.createQuery("from Owner", Owner.class);
+//        query.setHint("javax.persistence.fetchgraph", graph);
+
+        TypedQuery<Owner> query = em.createQuery("from Owner" +
+//                " o join fetch o.pets" +
+                "", Owner.class);
+        List<Owner> ownerlist = query.getResultList();
+        for (Owner o : ownerlist) {
+            o.getPets().size();
+        }
+
+        em.getTransaction().commit();
+        em.close();
+        long stop = System.nanoTime();
+
+        // stop time
+        System.out.println("To fetch this data from the database took " + (stop - start) / 1000000 + " milliseconds.");
+        System.exit(0);
+    }
+
+}
